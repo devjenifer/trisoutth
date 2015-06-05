688aeffa2f68c9f536e6dbda76d266ec048960e1
devjenifer <devjenifer@gmail.com>
2015-06-05 14:41:45 -0400

    Create content-single-event.php

---
+++ content-single-event.php
@@ -0,0 +1,6 @@
+<?php
+	espresso_get_template_part( 'content', 'espresso_events' );		
+?>
+
+<?php /*?><?php the_content(); ?><?php */?>
+<?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
